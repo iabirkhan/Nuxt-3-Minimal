870dace783035bd80a1f47b8942266f0de134b64
iabirkhan <113553754+iabirkhan@users.noreply.github.com>
2023-04-03 05:36:49 +0600

    Signed-off-by: iabirkhan <113553754+iabirkhan@users.noreply.github.com>

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "Nuxt 3 App",
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+    },
+  },
+});
